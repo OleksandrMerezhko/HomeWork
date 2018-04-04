3e919ec14e37146141603a7449124ef063ca79f9
OleksandrMerezhko <37825686+OleksandrMerezhko@users.noreply.github.com>
2018-04-04 11:55:46 +0300

    Массивы

--- L8_Massive4.java
+++ L8_Massive4.java
@@ -6,8 +6,9 @@ public class L8_Massive4 {
 		int[] a = {12, 43, 3, 76, 87, 92, 81, 32, 22, 66};
 		int s = 0;
 		
-		for (int i = 0; i < a.length; i++)
+		for (int i = 0; i < a.length; i++) {
 			s += a[i];
+		}
 		System.out.println((double)s/a.length);
 
 	}
