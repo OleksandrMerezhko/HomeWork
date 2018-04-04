bbb88092c7ab8be7fa5b005a85b17ec38907390c
OleksandrMerezhko <37825686+OleksandrMerezhko@users.noreply.github.com>
2018-04-04 12:06:20 +0300

    Массивы

---
+++ L8_Massive4_1.java
@@ -0,0 +1,10 @@
+package les1;
+
+public class L8_Massive4_1 {
+
+	public static void main(String[] args) {
+		int[] a = {12, 43, 3, 76, 87, 92, 81, 32, 22, 66};
+		System.out.println((a[0]+ a[1]+ a[2]+ a[3]+ a[4]+ a[5]+ a[6]+ a[7]+ a[8]+ a[9])/(double)10);
+	}
+
+}
