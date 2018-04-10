69c108d71cf879c8c889edc52dedfc84724d53d1
OleksandrMerezhko <37825686+OleksandrMerezhko@users.noreply.github.com>
2018-04-10 10:51:50 +0300

    Update L8_Massive4.java

--- L8_Massive4.java
+++ L8_Massive4.java
@@ -3,12 +3,12 @@ package les1;
 public class L8_Massive4 {
 
 	public static void main(String[] args) {
-		double[] a = {1200000000, 43, 3, 76, 87, 92, 81, 32, 2100000000, 66};
-		double s = 0;
+		double[] mas = {1200000000, 43, 3, 76, 87, 92, 81, 32, 2100000000, 66};
+		double sum = 0;
 		
-		for (int i = 0; i < a.length; i++) {
-			s += a[i];
+		for (int ind = 0; ind < mas.length; ind++) {
+			sum += mas[ind];
 		}
-		System.out.println(s/a.length);
+		System.out.println(sum/mas.length);
 	}
 }
