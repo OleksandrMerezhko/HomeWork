f4a10ae88ed7ead1e5015f057f965ac9f958c652
OleksandrMerezhko <37825686+OleksandrMerezhko@users.noreply.github.com>
2018-04-10 11:02:55 +0300

    Update L8_Massive3.java

--- L8_Massive3.java
+++ L8_Massive3.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public class L8_Massive3 {
 
 	public static void main(String[] args) {
-		int a[] = {23, 54, 76, 43, 91};
-		int b = a[0];
-		a[0] = a[a.length-1];
-		a[a.length-1] = b;
-		System.out.println(Arrays.toString(a));
+		int mas[] = {23, 54, 76, 43, 91};
+		int per = mas[0];
+		mas[0] = mas[mas.length-1];
+		mas[mas.length-1] = per;
+		String masprint = Arrays.toString(mas);
+		System.out.println(masprint);
 	}
-
 }
