4dac9388486c5b0d187a48fc40ae8a3028f39623
OleksandrMerezhko <37825686+OleksandrMerezhko@users.noreply.github.com>
2018-04-16 18:06:07 +0300

    S = x + y + z.
    Написать функцию, которая принимает в качестве аргументов одну строкуX, целое число Y и число с плавающей точкой Z и возвращает как результат строку в виде
    S = x + y + z.

---
+++ Method01.java
@@ -0,0 +1,14 @@
+package les1;
+
+public class Method01 {
+    
+    public static void function(String x,int y,double z){
+        System.out.println("S = " + x + y + z);
+    }
+    public static void main(String[] args){
+        String x = "do job ";
+        int y = 2018;
+        double z = 95.09;
+        function(x,y,z);
+    }
+}
